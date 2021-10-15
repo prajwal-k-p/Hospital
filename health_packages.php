44c8ee5b1a9dbd7a6fc6b5db2583b497146616b4
prajwal-k-p <prajwalkp1234@gmail.com>
2021-10-15 12:59:17 +0530

    commit

---
+++ health_packages.php
@@ -0,0 +1,228 @@
+
+    <?php
+  include("header.php")
+  ?>
+<main>
+<div class="slider-area slider-area2">
+        <div class="slider-active dot-style">
+          <!-- Slider Single -->
+          <div class="single-slider d-flex align-items-center slider-height2">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-xl-7 col-lg-8 col-md-10">
+                  <div class="hero-wrapper">
+                    <div class="hero__caption">
+                      <h1 data-animation="fadeInUp" data-delay=".3s">Health Packages</h1>
+                      <p data-animation="fadeInUp" data-delay=".6s">
+                        Almost before we knew it, we<br />
+                        had left the ground
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+  <section
+      class="wantToWork-area section-bg3"
+      data-background="assets/img/gallery/section_bg01.png"
+    >
+      <div class="container">
+        <div class="wants-wrapper w-padding2">
+          <div class="row align-items-center justify-content-between">
+            <div class="col-xl-7 col-lg-9 col-md-8">
+              <div class="wantToWork-caption wantToWork-caption2">
+                  <h1 style="color: rgb(47, 4, 202);">Health Packages :</h1>
+                
+              </div>
+            </div>
+            <div class="col-xl-2 col-lg-3 col-md-4">
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--? Services Area Start -->
+    <div class="service-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="single-cat text-center mb-50">
+            <div class="cat-icon">
+                <img src="assets/img/icon/services1.svg" alt="" />
+              </div>
+              <div class="cat-cap">
+                <h5><a href="services.html">General (Basic)Health Check-up INR. 1000/- </a></h5>
+                <div>
+                    <ul>
+                        <li> <h3>CBC & ESR </h3> </li>
+                        <li><h3>Blood Group</h3></li>
+                        <li><h3>Random Blood Sugar</h3> </li>
+                        <li><h3>S.Creatinine</h3> </li>
+                        <li> <h3>Blood Urea Nitrogen</h3>  </li>
+                        <li> <h3>Cholesterol</h3> </li>
+                        <li> <h3>ECG</h3></li>
+                        <li><h3>Chest X-ray</h3>  </li>
+                        <li> <h3>Consultation with Physician</h3></li>
+                    </ul>
+                  </div>
+                
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="single-cat text-center mb-50">
+              <div class="cat-icon">
+                <img src="assets/img/icon/services2.svg" alt="" />
+              </div>
+              <div class="cat-cap">
+                <h5><a href="services.html">Diabetic Health Check-up  INR. 1600/- </a></h5>
+                
+                  <ul>
+                      <li> <h3>Fasting Blood Sugar </h3> </li>
+                      <li><h3>Post Prandial  </h3></li>
+                      <li><h3>CBC & ESR        </h3> </li>
+                      <li><h3>HbA1c  </h3> </li>
+                      <li> <h3>S. Creatinine</h3>  </li>
+                      <li> <h3>Blood Urea Nitrogen  </h3> </li>
+                      <li> <h3>Urine Routine </h3></li>
+                      <li><h3>Lipid Profile</h3>  </li>
+                      <li> <h3>ECG</h3></li>
+                      <li> <h3>Consultation with Physician</h3></li>
+                  </ul>
+                
+                
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="single-cat text-center mb-50">
+              <div class="cat-icon">
+                <img src="assets/img/icon/services3.svg" alt="" />
+              </div>
+              <div class="cat-cap">
+                <h5><a href="services.html">Preventive Heart Checkup  INR. 2000/- </a></h5>
+                
+                  <ul>
+                    <li> <h3>CBC & ESR </h3> </li>
+                    <li><h3>Random Blood Sugar </h3></li>
+                    <li><h3>Lipid Profile </h3> </li>
+                    <li><h3>S. Creatinine  </h3> </li>
+                    <li> <h3>Blood Urea Nitrogen  </h3>  </li>
+                    <li> <h3>ECG </h3> </li>
+                    <li> <h3>2D ECHO / TMT </h3></li>
+                    <li><h3>Consultation With Cardiologist</h3>  </li>
+                    <!-- <li> <h3>ECG</h3></li>
+                    <li> <h3>Consultation with Physician</h3></li> -->
+                </ul>
+              
+              
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <section
+    class="wantToWork-area section-bg3"
+    data-background="assets/img/gallery/section_bg01.png"
+  >
+    <div class="container">
+      <div class="wants-wrapper w-padding2">
+        <div class="row align-items-center justify-content-between">
+          <div class="col-xl-7 col-lg-9 col-md-8">
+            <div class="wantToWork-caption wantToWork-caption2">
+              
+            </div>
+          </div>
+          <div class="col-xl-2 col-lg-3 col-md-4">
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--? Services Area Start -->
+  <div class="service-area">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-6">
+          <div class="single-cat text-center mb-50">
+            <div class="cat-icon">
+              <img src="assets/img/icon/services1.svg" alt="" />
+            </div>
+            <div class="cat-cap">
+              <h5><a href="services.html">Senior Citizen Health Packages INR. 2800/- </a></h5>
+              
+                <ul>
+                <li><h3>FBS & PPBS</h3></li>
+                <li><h3>CBC & ESR</h3></li>
+                 <li><h3>S. Cholesterol</h3></li> 
+                <li><h3>Blood Group & RH Typing</h3></li>
+                <li><h3>S. Creatinine</h3></li>
+                <li><h3>Calcium, Phosphate</h3></li>
+                <li><h3>HbA1c</h3></li>
+                <li><h3>BUN</h3></li>
+                <li><h3>Lipid Profile</h3></li>
+                <li><h3>ECG</h3></li>
+                <li><h3>Chest X – ray</h3></li>
+                <li><h3>USG Abd & Pelvis</h3></li>
+                <li><h3>PAP - Smear – Female</h3></li>
+                <li><h3>Consultation with Physician</h3></li>
+                </ul>
+              
+              
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6">
+          <div class="single-cat text-center mb-50">
+            <div class="cat-icon">
+              <img src="assets/img/icon/services2.svg" alt="" />
+            </div>
+            <div class="cat-cap">
+              <h5><a href="services.html">Master Health Checkup INR. 4200/-</a></h5>
+              
+                <ul>
+                  <li> <h3>Complete Hemogram</h3></li>
+                  <li><h3>Blood Group & RH Typing</h3> </li>
+                  <li><h3>FBS & PPBS</h3>  </li> 
+                  <li><h3>Renal Function Test</h3></li>
+                  <li><h3>Lipid Profile</h3> </li>
+                  <li><h3>Liver Function Test</h3></li>
+                  <li><h3>Urine Routine</h3> </li>
+                  <li><h3>Thyroid Function Test</h3></li>
+                  <li><h3>PAP – Smear - Female</h3></li>
+                  <li><h3>ECG</h3></li>
+                  <li><h3>Chest X – ray</h3></li>
+                  <li><h3>USG Abd & Pelvis</h3></li>
+                  <li><h3>ECHO & TMT</h3></li>
+                  <li><h3>Physiotherapy</h3></li>
+                  <li><h3>Consultation with Physician & Cardiologist</h3></li>
+                  </ul>
+              
+             
+            </div>
+          </div>
+        </div>
+        
+        </div>
+      </div>
+    </div>
+  </div>
+    <!-- Services Area End -->
+   
+    <!-- About Law End-->
+  </main>
+    
+
+    <?php
+    include("footer.php")
+    ?>
+
+    
+
